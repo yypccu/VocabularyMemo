415702a659892d8aa0e4638f68ce4bd58f9de988
Yen-Po <kk3398@hotmail.com>
2016-07-29 00:38:40 +0800

    Add a button for inserting vocabulary //TODO -- Bottom "Delete Word" -- Bottom "Edit Word" -- Sorting A-Z -- Sorting Word Attribute

--- app/src/main/java/com/yehyp/vocabularymemo/MainActivity.java
+++ app/src/main/java/com/yehyp/vocabularymemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.yehyp.vocabularymemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -25,6 +31,39 @@ public class MainActivity extends AppCompatActivity {
         vocabulary_list = vocabularyDAO.getAll();
         adapter = new MyAdapter(MainActivity.this, vocabulary_list);
         mListView.setAdapter(adapter);
+
+        Button addButtom = (Button)findViewById(R.id.button_add);
+        addButtom.setOnClickListener(addVocabuary);
     }
+
+    private OnClickListener addVocabuary = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openAddVocabularyDialog();
+        }
+    };
+
+
+    private void openAddVocabularyDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        final View inputView = inflater.inflate(R.layout.add_vocabulary, null);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setTitle("新增單字");
+        dialog.setView(inputView);
+        dialog.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ;
+            }
+        });
+        dialog.show();
+    }
+
 
 }
